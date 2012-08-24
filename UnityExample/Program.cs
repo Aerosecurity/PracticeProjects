d5e52043844586e6e59d0175ddb656d370562fd6
Christopher Reinschild <aerosecurity@gmail.com>
2012-08-24 11:30:38 -0400

    Started new project for learning Unity framework.

---
+++ UnityExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace UnityExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer IuC;
+        }
+    }
+
+    public static class IuC
+    {
+
+    }
+
+    public class FirstClass : IGetStringable
+    {
+        public String GetString()
+        {
+            return "First";
+        }
+    }
+
+    public class SecondClass : IGetStringable
+    {
+        public String GetString()
+        {
+            return "Second";
+        }
+    }
+
+    public interface IGetStringable
+    {
+        String GetString();
+    }
+}
