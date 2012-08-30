9be4434c7cc3cbd79d24924c93d1765f8e033586
Christopher Reinschild <aerosecurity@gmail.com>
2012-08-30 14:16:52 -0400

    created new projects to demonstrate unit testing.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class HelloWorld
+    {
+        public string GetMessage()
+        {
+            return "Hello World";
+        }
+
+        public static void Main(string[] args)
+        {
+            var hello = new HelloWorld();
+
+            var msg = hello.GetMessage();
+            Console.WriteLine(msg);
+            Console.ReadLine();
+            return;
+        }
+    }
+}
---
+++ HelloWorldTests/HelloWorldTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using HelloWorld;
+
+namespace HelloWorldTests
+{
+    [TestClass]
+    public class HelloWorldTests
+    {
+        [TestMethod]
+        public void GetMessage()
+        {
+            var hello = new HelloWorld.HelloWorld();
+
+            Assert.AreEqual(hello.GetMessage(), "Hello World");
+        }
+    }
+}
